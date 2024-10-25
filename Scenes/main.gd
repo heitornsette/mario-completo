cbfca1b05f4eded711a543439efcc9435edc2e7e
Hns <51509684+heitornsette@users.noreply.github.com>
2024-10-25 17:05:51 -0300

    Initial commit

---
+++ Scenes/main.gd
@@ -0,0 +1,13 @@
+extends Control
+
+func _on_botão_fase_1_pressed():
+	get_tree().change_scene_to_file("res://Scenes/Fase1.tscn")
+
+func _on_botão_fase_2_pressed():
+	get_tree().change_scene_to_file("res://Scenes/Fase2.tscn")
+
+func _on_botão_fase_3_pressed():
+	get_tree().change_scene_to_file("res://Scenes/Fase3.tscn")
+
+func _on_botão_fase_4_pressed():
+	get_tree().change_scene_to_file("res://Scenes/Fase4.tscn")
